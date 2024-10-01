89909ed40516fa76c03d93863280e50cd1a7950b
Wooksang Ryu <today8934@gmail.com>
2024-10-01 22:49:15 +0900

    feature: Layout

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,8 +1,5 @@
-import Navigation from "../../components/navigation";
-
 export default function AboutUs() {
     return <div>
-        <Navigation/>
         <h1>About us!</h1>
     </div>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import Navigation from "../components/navigation";
-
 export default function Tomato() {
     return <div>
-        <Navigation />
         <h1>Hello!</h1>
     </div>
 }
