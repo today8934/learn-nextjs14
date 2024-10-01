f637f2fb18bfd29c14de455e3a5f581fcca72509
Wooksang Ryu <today8934@gmail.com>
2024-10-01 23:14:09 +0900

    feature: Metadata

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,11 @@
-import Navigation from "../components/navigation";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'Not Found',
+}
 
 export default function NotFound() {
     return <div>
-        <Navigation />
         <h1>Not Found!</h1>
     </div>
 }
