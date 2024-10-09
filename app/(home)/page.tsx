d08b6ef3f4328b846ca27785837f3cb4b73ad1f1
Wooksang Ryu <today8934@gmail.com>
2024-10-09 10:04:24 +0900

    feature: Data Fetching on Client Side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,22 @@
-export const metadata = {
-    title: 'Home',
-}
+'use client';
 
-export default function Tomato() {
-    return <div>
-        <h1>Hello!</h1>
-    </div>
+import {useEffect, useState} from "react";
+
+export default function Page() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        const getMovies = async () => {
+            const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
+            const json = await response.json();
+            setMovies(json);
+            setIsLoading(false);
+        }
+        getMovies()
+    }, []);
+    return (
+        <div>
+            {isLoading ? "Loading..." : JSON.stringify(movies)}
+        </div>
+    )
 }
